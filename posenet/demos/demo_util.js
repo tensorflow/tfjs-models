d4022226ca66c1cf321e52136a2baedbf264cabf
oveddan <stangogh@gmail.com>
2018-04-06 16:59:42 -0400

    demos now work, and import directly from ts files

--- posenet/demos/demo_util.js
+++ posenet/demos/demo_util.js
@@ -15,99 +15,121 @@
  * =============================================================================
  */
 import * as tf from '@tensorflow/tfjs-core';
-import * as posenet from '../dist';
+import * as posenet from '../src';
 
-function toTuple({ y, x }) {
-    return [y, x];
+function toTuple({y, x}) {
+  return [y, x];
 }
 
 export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
-    ctx.beginPath();
-    ctx.moveTo(ax * scale, ay * scale);
-    ctx.lineTo(bx * scale, by * scale);
-    ctx.strokeStyle = color;
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.strokeStyle = color;
+  ctx.stroke();
 }
 
 export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
-    const adjacentKeyPoints = posenet.getAdjacentKeyPoints(keypoints, minConfidence);
-    adjacentKeyPoints.forEach((keypoints) => {
-        drawSegment(toTuple(keypoints[0].point), toTuple(keypoints[1].point), '#0000ff', scale, ctx);
-    });
+  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+    keypoints, minConfidence);
+
+  adjacentKeyPoints.forEach((keypoints) => {
+    drawSegment(toTuple(keypoints[0].point),
+      toTuple(keypoints[1].point), '#0000ff', scale, ctx);
+  });
 }
 
 export async function renderToCanvas(a, ctx) {
-    const [height, width,] = a.shape;
-    const imageData = new ImageData(width, height);
-    const data = await a.data();
-    for (let i = 0; i < height * width; ++i) {
-        const j = i * 4;
-        const k = i * 3;
-        imageData.data[j + 0] = data[k + 0];
-        imageData.data[j + 1] = data[k + 1];
-        imageData.data[j + 2] = data[k + 2];
-        imageData.data[j + 3] = 255;
-    }
-    ctx.putImageData(imageData, 0, 0);
+  const [height, width] = a.shape;
+  const imageData = new ImageData(width, height);
+
+  const data = await a.data();
+
+  for (let i = 0; i < height * width; ++i) {
+    const j = i * 4;
+    const k = i * 3;
+
+    imageData.data[j + 0] = data[k + 0];
+    imageData.data[j + 1] = data[k + 1];
+    imageData.data[j + 2] = data[k + 2];
+    imageData.data[j + 3] = 255;
+  }
+
+  ctx.putImageData(imageData, 0, 0);
 }
 
 export function renderImageToCanvas(image, size, canvas) {
-    canvas.width = size[0];
-    canvas.height = size[1];
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0);
+  canvas.width = size[0];
+  canvas.height = size[1];
+  const ctx = canvas.getContext('2d');
+
+  ctx.drawImage(image, 0, 0);
 }
 
 export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
-    const ctx = canvas.getContext('2d');
-    const radius = 5;
-    const scaledValues = heatMapValues.mul(tf.scalar(outputStride, 'int32'));
-    drawPoints(ctx, scaledValues, radius, 'blue');
+  const ctx = canvas.getContext('2d');
+  const radius = 5;
+  const scaledValues = heatMapValues.mul(tf.scalar(outputStride, 'int32'));
+
+  drawPoints(ctx, scaledValues, radius, 'blue');
 }
 
 function drawPoints(ctx, points, radius, color) {
-    const data = points.buffer().values;
-    for (let i = 0; i < data.length; i += 2) {
-        const pointY = data[i];
-        const pointX = data[i + 1];
-        if (pointX !== 0 && pointY !== 0) {
-            ctx.beginPath();
-            ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
-            ctx.fillStyle = 'blue';
-            ctx.fill();
-        }
+  const data = points.buffer().values;
+
+  for (let i = 0; i < data.length; i += 2) {
+    const pointY = data[i];
+    const pointX = data[i + 1];
+
+    if (pointX !== 0 && pointY !== 0) {
+      ctx.beginPath();
+      ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = 'blue';
+      ctx.fill();
     }
+  }
 }
 
-export function drawOffsetVectors(heatMapValues, offsets, outputStride, scale = 1, ctx) {
-    const offsetPoints = posenet.singlePose.getOffsetPoints(heatMapValues, outputStride, offsets);
-    const heatmapData = heatMapValues.buffer().values;
-    const offsetPointsData = offsetPoints.buffer().values;
-    for (let i = 0; i < heatmapData.length; i += 2) {
-        const heatmapY = heatmapData[i] * outputStride;
-        const heatmapX = heatmapData[i + 1] * outputStride;
-        const offsetPointY = offsetPointsData[i];
-        const offsetPointX = offsetPointsData[i + 1];
-        drawSegment([heatmapY, heatmapX], [offsetPointY, offsetPointX], '#ffff00', scale, ctx);
-    }
+export function drawOffsetVectors(
+  heatMapValues, offsets, outputStride, scale = 1, ctx) {
+  const offsetPoints = posenet.singlePose.getOffsetPoints(
+    heatMapValues, outputStride, offsets);
+
+  const heatmapData = heatMapValues.buffer().values;
+  const offsetPointsData = offsetPoints.buffer().values;
+
+  for (let i = 0; i < heatmapData.length; i += 2) {
+    const heatmapY = heatmapData[i] * outputStride;
+    const heatmapX = heatmapData[i + 1] * outputStride;
+    const offsetPointY = offsetPointsData[i];
+    const offsetPointX = offsetPointsData[i + 1];
+
+    drawSegment([heatmapY, heatmapX], [offsetPointY, offsetPointX],
+      '#ffff00', scale, ctx);
+  }
 }
 
 export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
-    for (let i = 0; i < keypoints.length; i++) {
-        const keypoint = keypoints[i];
-        if (keypoint.score < minConfidence) {
-            continue;
-        }
-        const { y, x } = keypoint.point;
-        ctx.beginPath();
-        ctx.arc(x * scale, y * scale, 3, 0, 2 * Math.PI);
-        ctx.fillStyle = 'blue';
-        ctx.fill();
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
+
+    if (keypoint.score < minConfidence) {
+      continue;
     }
+
+    const {y, x} = keypoint.point;
+    ctx.beginPath();
+    ctx.arc(x * scale, y * scale, 3, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+  }
 }
 
 export function drawBoundingBox(keypoints, ctx) {
-    const boundingBox = posenet.getBoundingBox(keypoints);
-    ctx.rect(boundingBox.minX, boundingBox.minY, boundingBox.maxX - boundingBox.minX, boundingBox.maxY - boundingBox.minY);
-    ctx.stroke();
+  const boundingBox = posenet.getBoundingBox(keypoints);
+
+  ctx.rect(boundingBox.minX, boundingBox.minY,
+    boundingBox.maxX - boundingBox.minX, boundingBox.maxY - boundingBox.minY);
+
+  ctx.stroke();
 }
