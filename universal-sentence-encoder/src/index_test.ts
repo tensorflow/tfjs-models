c37e8bd4bfea54d15d4e0f4f2ee95e4810a39753
Ann Yuan <annyuan@google.com>
2019-02-04 11:23:03 -0500

    add more tests to tokenizer

--- universal-sentence-encoder/src/index_test.ts
+++ universal-sentence-encoder/src/index_test.ts
@@ -18,25 +18,54 @@ import * as tf from '@tensorflow/tfjs';
 import {describeWithFlags} from '@tensorflow/tfjs-core/dist/jasmine_util';
 import {Tokenizer} from './tokenizer';
 
-describeWithFlags('Universal Sentence Encoder', tf.test_util.NODE_ENVS, () => {
-  let tokenizer: Tokenizer;
-  beforeAll(() => {
-    tokenizer = new Tokenizer([
-      ['�', 0], ['<s>', 0], ['</s>', 0], ['extra_token_id_1', 0],
-      ['extra_token_id_2', 0], ['extra_token_id_3', 0], ['▁', -1], ['▁a', -1],
-      ['▁ç', -2], ['a', -3]
-    ]);
-  });
+describeWithFlags(
+    'Universal Sentence Encoder tokenizer', tf.test_util.NODE_ENVS, () => {
+      let tokenizer: Tokenizer;
+      beforeAll(() => {
+        tokenizer = new Tokenizer([
+          ['�', 0],
+          ['<s>', 0],
+          ['</s>', 0],
+          ['extra_token_id_1', 0],
+          ['extra_token_id_2', 0],
+          ['extra_token_id_3', 0],
+          ['▁', -2],
+          ['▁a', -1],
+          ['▁ç', -2],
+          ['a', -3],
+          ['.', -1],
+          ['▁I', -1],
+          ['▁like', -1],
+          ['▁it', -1],
+          ['I', -2],
+          ['like', -2],
+          ['it', -2],
+          ['l', -3],
+          ['i', -3],
+          ['k', -3],
+          ['e', -3],
+          ['i', -3],
+          ['t', -3]
+        ]);
+      });
 
-  it('should normalize inputs', () => {
-    expect(tokenizer.encode('ça')).toEqual(tokenizer.encode('c\u0327a'));
-  });
+      it('basic usage', () => {
+        expect(tokenizer.encode('Ilikeit.')).toEqual([11, 15, 16, 10]);
+      });
 
-  it('should handle unknown inputs', () => {
-    expect(() => tokenizer.encode('😹')).not.toThrow();
-  });
+      it('handles whitespace', () => {
+        expect(tokenizer.encode('I like it.')).toEqual([11, 12, 13, 10]);
+      });
 
-  it('should treat consecutive unknown inputs as a single word', () => {
-    expect(tokenizer.encode('a😹😹')).toEqual([7, 0]);
-  });
-});
+      it('should normalize inputs', () => {
+        expect(tokenizer.encode('ça')).toEqual(tokenizer.encode('c\u0327a'));
+      });
+
+      it('should handle unknown inputs', () => {
+        expect(() => tokenizer.encode('😹')).not.toThrow();
+      });
+
+      it('should treat consecutive unknown inputs as a single word', () => {
+        expect(tokenizer.encode('a😹😹')).toEqual([7, 0]);
+      });
+    });
