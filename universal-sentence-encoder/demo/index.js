0f312b6ce3f9282633493b5ddea43d737fe67bfa
Ann Yuan <annyuan@google.com>
2019-02-06 10:46:16 -0500

    basic usage

--- universal-sentence-encoder/demo/index.js
+++ universal-sentence-encoder/demo/index.js
@@ -1,1 +1,18 @@
-console.log('lol');
+import * as use from '@tensorflow-models/universal-sentence-encoder';
+
+const sentences = [
+  'I like my phone.', 'Your cellphone looks great.', 'Will it snow tomorrow?',
+  'Recently a lot of hurricanes have hit the US.',
+  'An apple a day, keeps the doctors away.', 'Eating strawberries is healthy.'
+];
+
+const init = async () => {
+  const model = await use.load();
+
+  document.querySelector('#loading').style.display = 'none';
+
+  const embeddings = await model.embed(sentences);
+  console.log(embeddings);
+};
+
+init();
