47b8b973dde19ededb39b1f325828feb04b77890
Ann Yuan <annyuan@google.com>
2019-02-07 06:21:16 -0500

    pr comments

--- universal-sentence-encoder/demo/index.js
+++ universal-sentence-encoder/demo/index.js
@@ -1,13 +1,22 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
 import * as use from '@tensorflow-models/universal-sentence-encoder';
 import {interpolateReds} from 'd3-scale-chromatic';
-
-const dot = (v1, v2) => {
-  let product = 0;
-  for (let i = 0; i < v1.length; i++) {
-    product += v1[i] * v2[i];
-  }
-  return product;
-};
 
 const sentences = [
   'I like my phone.', 'Your cellphone looks great.', 'How old are you?',
@@ -15,26 +24,13 @@ const sentences = [
   'Eating strawberries is healthy.'
 ];
 
-const renderSentences = () => {
-  sentences.forEach((sentence, i) => {
-    const sentenceDom = document.createElement('div');
-    sentenceDom.textContent = `${i + 1}) ${sentence}`;
-    document.querySelector('#sentences-container').appendChild(sentenceDom);
-  });
-};
-
 const init = async () => {
   const model = await use.load();
 
-  renderSentences();
   document.querySelector('#loading').style.display = 'none';
+  renderSentences();
 
   const embeddings = await model.embed(sentences);
-  const embeddingsData = embeddings.dataSync();
-  const embeddingsArr = [];
-  for (let i = 0; i < sentences.length; i++) {
-    embeddingsArr.push(embeddingsData.slice(i * 512, i * 512 + 512));
-  }
 
   const matrixSize = 250;
   const cellSize = matrixSize / sentences.length;
@@ -60,7 +56,13 @@ const init = async () => {
     yLabelsContainer.appendChild(labelYDom);
 
     for (let j = i; j < sentences.length; j++) {
-      let score = dot(embeddingsArr[i], embeddingsArr[j]);
+      const sentenceI = embeddings.slice([i, 0], [1]);
+      const sentenceJ = embeddings.slice([j, 0], [1]);
+      const sentenceITranspose = false;
+      const sentenceJTransepose = true;
+      const score =
+          sentenceI.matMul(sentenceJ, sentenceITranspose, sentenceJTransepose)
+              .dataSync();
 
       ctx.fillStyle = interpolateReds(score);
       ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
@@ -70,3 +72,11 @@ const init = async () => {
 };
 
 init();
+
+const renderSentences = () => {
+  sentences.forEach((sentence, i) => {
+    const sentenceDom = document.createElement('div');
+    sentenceDom.textContent = `${i + 1}) ${sentence}`;
+    document.querySelector('#sentences-container').appendChild(sentenceDom);
+  });
+};
