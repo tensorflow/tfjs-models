5972646f57f9c46c5b1a3da925bd101a5f08f252
Tyler Zhu <tylerzhu@google.com>
2019-06-07 14:27:38 -0400

    updates ResNet50 and MobileNet checkpoint URL.

--- posenet/src/checkpoints.ts
+++ posenet/src/checkpoints.ts
@@ -15,13 +15,15 @@
  * =============================================================================
  */
 
-const MOBILENET_BASE_URL = 'http://localhost:8080/';
-const RESNET50_BASE_URL = 'http://localhost:8080/';
+const MOBILENET_BASE_URL =
+    'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/mobilenet/';
+const RESNET50_BASE_URL =
+    'https://storage.googleapis.com/tfjs-models/savedmodel/posenet/resnet50/';
 
 // The PoseNet 2.0 ResNet50 models use the latest TensorFlow.js 1.0 model
 // format.
 export function resNet50Checkpoint(stride: number, quantBytes: number): string {
-  const graphJson = `resnet50-stride${stride}/model.json`;
+  const graphJson = `model-stride${stride}.json`;
   // quantBytes=4 corresponding to the non-quantized full-precision checkpoints.
   if (quantBytes == 4) {
     return RESNET50_BASE_URL + `float/` + graphJson;
@@ -36,11 +38,12 @@ export function mobileNetCheckpoint(
     resolution: number, stride: number, multiplier: number,
     quantBytes: number): string {
   const toStr: {[key: number]: string} = {1.0: '100', 0.75: '075', 0.50: '050'};
-  const graphJson = `mobilenet-${toStr[multiplier]}-stride${stride}/model.json`;
+  const graphJson = `model-stride${stride}.json`;
   // quantBytes=4 corresponding to the non-quantized full-precision checkpoints.
   if (quantBytes == 4) {
-    return MOBILENET_BASE_URL + `float/` + graphJson;
+    return MOBILENET_BASE_URL + `float/${toStr[multiplier]}/` + graphJson;
   } else {
-    return MOBILENET_BASE_URL + `quant${quantBytes}/` + graphJson;
+    return MOBILENET_BASE_URL + `quant${quantBytes}/${toStr[multiplier]}/` +
+        graphJson;
   }
 }
