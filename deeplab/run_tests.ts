37a973a636f7cd95201f18b755539ed2a9d6e721
sdll <sasha@sdll.space>
2019-06-12 13:52:49 +0300

    fix: add cosmetic changes

--- deeplab/run_tests.ts
+++ deeplab/run_tests.ts
@@ -16,6 +16,6 @@
  */
 
 import * as jasmine_util from '@tensorflow/tfjs-core/dist/jasmine_util';
-import {runTests} from '../test_util';
+import { runTests } from '../test_util';
 
 runTests(jasmine_util);
