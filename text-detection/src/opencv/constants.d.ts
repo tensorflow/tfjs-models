a9ba1526ef1dcf1ff54d9df8c807daeee1bd8905
Sasha Illarionov <sasha@sdll.space>
2019-07-13 09:22:39 +0300

    fix: add apache license

--- text-detection/src/opencv/constants.d.ts
+++ text-detection/src/opencv/constants.d.ts
@@ -1,3 +1,41 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+// MIT License
+// Copyright (c) 2017 Vincent Mühler
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 export const CV_8U: number;
 export const CV_8S: number;
 export const CV_16U: number;
@@ -563,25 +601,15 @@ export const LBP_FRONTALFACE_IMPROVED: string;
 export const LBP_PROFILEFACE: string;
 export const LBP_SILVERWARE: string;
 
-export const termCriteria: {
-  COUNT: number;
-  MAX_ITER: number;
-  EPS: number;
-}
+export const termCriteria: {COUNT: number; MAX_ITER: number; EPS: number;}
 
 export const ml: {
-  COL_SAMPLE: number;
-  ROW_SAMPLE: number;
-  VAR_CATEGORICAL: number;
+  COL_SAMPLE: number; ROW_SAMPLE: number; VAR_CATEGORICAL: number;
   VAR_NUMERICAL: number;
   VAR_ORDERED: number;
 
   SVM: {
-    CUSTOM: number;
-    LINEAR: number;
-    POLY: number;
-    RBF: number;
-    SIGMOID: number;
+    CUSTOM: number; LINEAR: number; POLY: number; RBF: number; SIGMOID: number;
     CHI2: number;
     INTER: number;
     C: number;
@@ -594,8 +622,6 @@ export const ml: {
 }
 
 export const statModel: {
-  COMPRESSED_INPUT: number;
-  PREPROCESSED_INPUT: number;
-  RAW_OUTPUT: number;
+  COMPRESSED_INPUT: number; PREPROCESSED_INPUT: number; RAW_OUTPUT: number;
   UPDATE_MODEL: number;
 }
