ab5ce8e52dfeaa781420f28fff1830f097389f47
Ann Yuan <annyuan@gmail.com>
2019-07-23 17:44:43 -0400

    clean build

--- facemesh/src/layers.ts
+++ facemesh/src/layers.ts
@@ -2,10 +2,10 @@ import * as tf from '@tensorflow/tfjs-core';
 import * as tfl from '@tensorflow/tfjs-layers';
 
 function getExactlyOneTensor(xs: tf.Tensor[]) {
-  var x;
+  let x;
   if (Array.isArray(xs)) {
     if (xs.length !== 1) {
-      throw new ValueError('Expected Tensor length to be 1; got ' + xs.length);
+      throw new Error(`Expected Tensor length to be 1; got ${xs.length}.`);
     }
     x = xs[0];
   } else {
@@ -15,25 +15,26 @@ function getExactlyOneTensor(xs: tf.Tensor[]) {
 }
 
 class ChannelPadding extends tfl.layers.Layer {
-  static className = 'ChannelPadding';
+  private padding: number;
+  // private mode: string;
 
-  constructor(config) {
+  constructor(config: any) {
     super({});
 
     this.padding = config.padding;
-    this.mode = config.mode;
+    // this.mode = config.mode;
   }
 
-  computeOutputShape(inputShape) {
+  computeOutputShape(inputShape: number[]) {
     let batch = inputShape[0], dim1 = inputShape[1], dim2 = inputShape[2];
     let values = inputShape[3];
     let new_shape = [batch, dim1, dim2, values + this.padding];
     return new_shape;
   }
 
-  call(inputs, kwargs) {
+  call(inputs: tf.Tensor[]) {
     let input = getExactlyOneTensor(inputs);
-    return tf.pad(input, [[0, 0], [0, 0], [0, 0], [0, this.padding]], 0.0)
+    return tf.pad(input, [[0, 0], [0, 0], [0, 0], [0, this.padding]], 0.0);
   }
 
   static get className() {
