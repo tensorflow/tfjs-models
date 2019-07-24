631f830754f0a4b9dab6957c2cea51e01a4a5f9d
Ann Yuan <annyuan@gmail.com>
2019-07-24 08:41:10 -0400

    types

--- facemesh/src/face.ts
+++ facemesh/src/face.ts
@@ -1,14 +1,36 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
 import * as tf from '@tensorflow/tfjs-core';
 import * as tfl from '@tensorflow/tfjs-layers';
 
 import {Box} from './box';
 
+type AnchorsConfig = {
+  strides: [number, number],
+  anchors: [number, number]
+};
+
 export class BlazeFaceModel {
   private blazeFaceModel: tfl.LayersModel;
   private width: number;
   private height: number;
-  private config: any;
-  private anchors: any;
+  private config: AnchorsConfig;
+  private anchors: tf.Tensor;
   private inputSize: tf.Tensor;
   private iouThreshold: number;
   private scoreThreshold: number;
@@ -17,22 +39,23 @@ export class BlazeFaceModel {
     this.blazeFaceModel = model;
     this.width = width;
     this.height = height;
-    this.config = this._get_anchors_config();
-    this.anchors = this._generate_anchors(width, height, this.config);
+    this.config = this.getAnchorsConfig();
+    this.anchors = this.generateAnchors(width, height, this.config);
     this.inputSize = tf.tensor([width, height]);
 
     this.iouThreshold = 0.3;
     this.scoreThreshold = 0.75;
   }
 
-  _get_anchors_config() {
+  private getAnchorsConfig(): AnchorsConfig {
     return {
       'strides': [8, 16],
       'anchors': [2, 6],
     };
-  };
+  }
 
-  _generate_anchors(width: number, height: number, outputSpec: any) {
+  private generateAnchors(
+      width: number, height: number, outputSpec: AnchorsConfig): tf.Tensor {
     const anchors = [];
     for (let i = 0; i < outputSpec.strides.length; ++i) {
       const stride = outputSpec.strides[i];
@@ -51,20 +74,21 @@ export class BlazeFaceModel {
         }
       }
     }
+
     return tf.tensor(anchors);
   }
 
-  _decode_bounds(boxOutputs: tf.Tensor) {
-    const box_starts = tf.slice(boxOutputs, [0, 0], [-1, 2]);
-    const centers = tf.add(box_starts, this.anchors);
+  private decodeBounds(boxOutputs: tf.Tensor2D): tf.Tensor2D {
+    const boxStarts = tf.slice(boxOutputs, [0, 0], [-1, 2]);
+    const centers = tf.add(boxStarts, this.anchors);
 
-    const box_sizes = tf.slice(boxOutputs, [0, 2], [-1, 2]);
+    const boxSizes = tf.slice(boxOutputs, [0, 2], [-1, 2]);
 
-    const box_sizes_norm = tf.div(box_sizes, this.inputSize);
-    const centers_norm = tf.div(centers, this.inputSize);
+    const boxSizesNormalized = tf.div(boxSizes, this.inputSize);
+    const centersNormalized = tf.div(centers, this.inputSize);
 
-    const starts = tf.sub(centers_norm, tf.div(box_sizes_norm, 2));
-    const ends = tf.add(centers_norm, tf.div(box_sizes_norm, 2));
+    const starts = tf.sub(centersNormalized, tf.div(boxSizesNormalized, 2));
+    const ends = tf.add(centersNormalized, tf.div(boxSizesNormalized, 2));
 
     return tf.concat2d(
         [
@@ -74,47 +98,46 @@ export class BlazeFaceModel {
         1);
   }
 
-  _getBoundingBox(inputImage: tf.Tensor) {
-    const img = tf.mul(tf.sub(inputImage, 0.5), 2);  // make input [-1, 1]
-
-    const detectOutputs = this.blazeFaceModel.predict(img);
+  private getBoundingBox(inputImage: tf.Tensor4D): number[][] {
+    const normalizedImage = tf.mul(tf.sub(inputImage, 0.5), 2);
+    const detectOutputs = this.blazeFaceModel.predict(normalizedImage);
 
     const scores =
         tf.sigmoid(
               tf.slice(detectOutputs as tf.Tensor3D, [0, 0, 0], [1, -1, 1]))
             .squeeze();
 
-    const box_regressors =
+    const boxRegressors =
         tf.slice(detectOutputs as tf.Tensor3D, [0, 0, 1], [1, -1, 4]).squeeze();
-    const boxes = this._decode_bounds(box_regressors);
-    const box_indices = tf.image
-                            .nonMaxSuppression(
-                                boxes, scores as tf.Tensor1D, 1,
-                                this.iouThreshold, this.scoreThreshold)
-                            .arraySync();
+    const boxes = this.decodeBounds(boxRegressors as tf.Tensor2D);
+    const boxIndices = tf.image
+                           .nonMaxSuppression(
+                               boxes, scores as tf.Tensor1D, 1,
+                               this.iouThreshold, this.scoreThreshold)
+                           .arraySync();
 
-    if (box_indices.length == 0) {
+    if (boxIndices.length === 0) {
       return null;  // TODO (vakunov): don't return null. Empty box?
     }
 
     // TODO (vakunov): change to multi face case
-    const box_index = box_indices[0];
-    const result_box = tf.slice(boxes, [box_index, 0], [1, -1]);
-    return result_box.arraySync();
+    const boxIndex = boxIndices[0];
+    const resultBox = tf.slice(boxes, [boxIndex, 0], [1, -1]);
+    return resultBox.arraySync();
   }
 
-  getSingleBoundingBox(inputImage: tf.Tensor4D) {
-    const original_h = inputImage.shape[1];
-    const original_w = inputImage.shape[2];
+  getSingleBoundingBox(inputImage: tf.Tensor4D): Box {
+    const originalHeight = inputImage.shape[1];
+    const originalWidth = inputImage.shape[2];
 
     const image = inputImage.resizeBilinear([this.width, this.height]).div(255);
-    const bboxes = this._getBoundingBox(image);
+    const bboxes = this.getBoundingBox(image as tf.Tensor4D);
 
     if (!bboxes) {
       return null;
     }
 
-    const factors = tf.div([original_w, original_h], this.inputSize);
+    const factors = tf.div([originalWidth, originalHeight], this.inputSize);
     return new Box(tf.tensor(bboxes)).scale(factors as tf.Tensor1D);
   }
 }
