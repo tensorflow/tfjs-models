be3d6b7b35d39c8795b5a0bb4a7e573b56f86a11
Ann Yuan <annyuan@gmail.com>
2019-07-24 07:42:15 -0400

    update

--- facemesh/demo/index.js
+++ facemesh/demo/index.js
@@ -1,6 +1,26 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
 import * as faceMesh from '@tensorflow-models/facemesh';
+import Stats from 'stats.js';
 
 let model, ctx, videoWidth, videoHeight, video, canvas;
+
+const stats = new Stats();
 
 async function setupCamera() {
   video = document.getElementById('video');
@@ -18,8 +38,17 @@ async function setupCamera() {
   });
 }
 
+/**
+ * Sets up a frames per second panel on the top-left of the window
+ */
+function setupFPS() {
+  stats.showPanel(0);  // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.getElementById('main').appendChild(stats.dom);
+}
+
 const renderPrediction =
     async () => {
+  stats.begin();
   const prediction = await model.estimateFace(video);
   if (prediction) {
     const keypoints = prediction.mesh;
@@ -38,6 +67,8 @@ const renderPrediction =
       ctx.fill();
     }
   }
+
+  stats.end();
 
   requestAnimationFrame(renderPrediction);
 }
@@ -60,6 +91,8 @@ const setupPage =
   ctx.scale(-1, 1);
 
   model = await faceMesh.load();
+
+  setupFPS();
 
   renderPrediction();
 }
