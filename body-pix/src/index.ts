7117271163cd71806da0c165928b671280bf591f
Tyler Zhu <tylerzhu@google.com>
2019-08-12 17:33:46 -0400

    Fixes index.ts

--- body-pix/src/index.ts
+++ body-pix/src/index.ts
@@ -16,7 +16,7 @@
  */
 
 export {BodyPix, load} from './body_pix_model';
-export {Checkpoint, checkpoints} from './checkpoints';
+export {mobileNetCheckpoint, resNet50Checkpoint} from './checkpoints';
 export {decodePartSegmentation, toMask} from './decode_part_map';
 export {drawBokehEffect, drawMask, drawPixelatedMask, toColoredPartImageData, toMaskImageData} from './output_rendering_util';
 export {partChannels} from './part_channels';
