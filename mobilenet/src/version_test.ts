393816a07c62b178ce06afa48852d5474b3b091c
Nikhil Thorat <nsthorat@google.com>
2019-12-06 11:11:12 -0500

    save

--- mobilenet/src/version_test.ts
+++ mobilenet/src/version_test.ts
@@ -18,7 +18,7 @@
 import {version} from './index';
 
 describe('version', () => {
-  it('version is contained', () => {
+  it('version matches package.json', () => {
     // tslint:disable-next-line:no-require-imports
     const expected = require('../package.json').version;
     expect(version).toBe(expected);
