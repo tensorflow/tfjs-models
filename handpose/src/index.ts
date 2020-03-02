993d57ce9bbefc2faf31967060a7913c7785af2d
Ann Yuan <annyuan@gmail.com>
2020-03-02 12:43:42 -0500

    add docs

--- handpose/src/index.ts
+++ handpose/src/index.ts
@@ -20,16 +20,17 @@ import * as tf from '@tensorflow/tfjs-core';
 import {HandDetector} from './hand';
 import {HandPipeline} from './pipeline';
 
+// Load the bounding box detector model.
 async function loadHandDetectorModel() {
   const HANDDETECT_MODEL_PATH =
       'https://tfhub.dev/tensorflow/tfjs-model/handdetector/1/default/1';
-
   return tfconv.loadGraphModel(HANDDETECT_MODEL_PATH, {fromTFHub: true});
 }
 
 const MESH_MODEL_INPUT_WIDTH = 256;
 const MESH_MODEL_INPUT_HEIGHT = 256;
 
+// Load the mesh detector model.
 async function loadHandMeshModel() {
   const HANDPOSE_MODEL_PATH =
       'https://tfhub.dev/tensorflow/tfjs-model/handskeleton/1/default/1';
@@ -46,6 +47,21 @@ async function loadAnchors() {
       .then(d => d.json());
 }
 
+/**
+ * Load handpose.
+ *
+ * @param config A configuration object with the following properties:
+ *  `maxContinuousChecks` How many frames to go without running the bounding box
+ * detector. Defaults to infinity. Set to a lower value if you want a safety net
+ * in case the mesh detector produces consistently flawed predictions.
+ *  `detectionConfidence` Threshold for discarding a prediction. Defaults to
+ * 0.8.
+ *  `iouThreshold` A float representing the threshold for deciding whether boxes
+ * overlap too much in non-maximum suppression. Must be between [0, 1]. Defaults
+ * to 0.3.
+ *  `scoreThreshold` A threshold for deciding when to remove boxes based
+ * on score in non-maximum suppression. Defaults to 0.75.
+ */
 export async function load({
   maxContinuousChecks = Infinity,
   detectionConfidence = 0.8,
