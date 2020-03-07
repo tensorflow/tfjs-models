81f35ab3b1b4c6968dcbb0461c3caa1b7c07aad2
Ann Yuan <annyuan@gmail.com>
2020-03-07 09:24:47 -0500

    [facemesh] Add model. (#413)

---
+++ facemesh/demo/index.js
@@ -0,0 +1,211 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as facemesh from '@tensorflow-models/facemesh';
+import Stats from 'stats.js';
+import * as tf from '@tensorflow/tfjs-core';
+import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
+// TODO(annxingyuan): read version from tfjsWasm directly once
+// https://github.com/tensorflow/tfjs/pull/2819 is merged.
+import {version} from '@tensorflow/tfjs-backend-wasm/dist/version';
+
+import {TRIANGULATION} from './triangulation';
+
+tfjsWasm.setWasmPath(
+    `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${
+        version}/dist/tfjs-backend-wasm.wasm`);
+
+function isMobile() {
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return isAndroid || isiOS;
+}
+
+function drawPath(ctx, points, closePath) {
+  const region = new Path2D();
+  region.moveTo(points[0][0], points[0][1]);
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    region.lineTo(point[0], point[1]);
+  }
+
+  if (closePath) {
+    region.closePath();
+  }
+  ctx.stroke(region);
+}
+
+let model, ctx, videoWidth, videoHeight, video, canvas,
+    scatterGLHasInitialized = false, scatterGL;
+
+const VIDEO_SIZE = 500;
+const mobile = isMobile();
+// Don't render the point cloud on mobile in order to maximize performance and
+// to avoid crowding limited screen space.
+const renderPointcloud = mobile === false;
+const stats = new Stats();
+const state = {
+  backend: 'wasm',
+  maxFaces: 1,
+  triangulateMesh: true
+};
+
+if (renderPointcloud) {
+  state.renderPointcloud = true;
+}
+
+function setupDatGui() {
+  const gui = new dat.GUI();
+  gui.add(state, 'backend', ['wasm', 'webgl', 'cpu'])
+      .onChange(async backend => {
+        await tf.setBackend(backend);
+      });
+
+  gui.add(state, 'maxFaces', 1, 20, 1).onChange(async val => {
+    model = await facemesh.load({maxFaces: val});
+  });
+
+  gui.add(state, 'triangulateMesh');
+
+  if (renderPointcloud) {
+    gui.add(state, 'renderPointcloud').onChange(render => {
+      document.querySelector('#scatter-gl-container').style.display =
+          render ? 'inline-block' : 'none';
+    });
+  }
+}
+
+async function setupCamera() {
+  video = document.getElementById('video');
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      facingMode: 'user',
+      // Only setting the video to a specified size in order to accommodate a
+      // point cloud, so on mobile devices accept the default size.
+      width: mobile ? undefined : VIDEO_SIZE,
+      height: mobile ? undefined : VIDEO_SIZE
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function renderPrediction() {
+  stats.begin();
+
+  const predictions = await model.estimateFaces(video);
+  ctx.drawImage(
+      video, 0, 0, videoWidth, videoHeight, 0, 0, canvas.width, canvas.height);
+
+  if (predictions.length > 0) {
+    predictions.forEach(prediction => {
+      const keypoints = prediction.scaledMesh;
+
+      if (state.triangulateMesh) {
+        for (let i = 0; i < TRIANGULATION.length / 3; i++) {
+          const points = [
+            TRIANGULATION[i * 3], TRIANGULATION[i * 3 + 1],
+            TRIANGULATION[i * 3 + 2]
+          ].map(index => keypoints[index]);
+
+          drawPath(ctx, points, true);
+        }
+      } else {
+        for (let i = 0; i < keypoints.length; i++) {
+          const x = keypoints[i][0];
+          const y = keypoints[i][1];
+
+          ctx.beginPath();
+          ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
+          ctx.fill();
+        }
+      }
+    });
+
+    if (renderPointcloud && state.renderPointcloud && scatterGL != null) {
+      const pointsData = predictions.map(prediction => {
+        let scaledMesh = prediction.scaledMesh;
+        return scaledMesh.map(point => ([-point[0], -point[1], -point[2]]));
+      });
+
+      let flattenedPointsData = [];
+      for (let i = 0; i < pointsData.length; i++) {
+        flattenedPointsData = flattenedPointsData.concat(pointsData[i]);
+      }
+      const dataset = new ScatterGL.Dataset(flattenedPointsData);
+
+      if (!scatterGLHasInitialized) {
+        scatterGL.render(dataset);
+      } else {
+        scatterGL.updateDataset(dataset);
+      }
+      scatterGLHasInitialized = true;
+    }
+  }
+
+  stats.end();
+  requestAnimationFrame(renderPrediction);
+};
+
+async function main() {
+  await tf.setBackend(state.backend);
+  setupDatGui();
+
+  stats.showPanel(0);  // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.getElementById('main').appendChild(stats.dom);
+
+  await setupCamera();
+  video.play();
+  videoWidth = video.videoWidth;
+  videoHeight = video.videoHeight;
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  canvas = document.getElementById('output');
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+  const canvasContainer = document.querySelector('.canvas-wrapper');
+  canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+
+  ctx = canvas.getContext('2d');
+  ctx.translate(canvas.width, 0);
+  ctx.scale(-1, 1);
+  ctx.fillStyle = '#32EEDB';
+  ctx.strokeStyle = '#32EEDB';
+  ctx.lineWidth = 0.5;
+
+  model = await facemesh.load({maxFaces: state.maxFaces});
+  renderPrediction();
+
+  if (renderPointcloud) {
+    document.querySelector('#scatter-gl-container').style =
+        `width: ${VIDEO_SIZE}px; height: ${VIDEO_SIZE}px;`;
+
+    scatterGL = new ScatterGL(
+        document.querySelector('#scatter-gl-container'),
+        {'rotateOnStart': false, 'selectEnabled': false});
+  }
+};
+
+main();
---
+++ facemesh/src/index.ts
@@ -0,0 +1,295 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as blazeface from '@tensorflow-models/blazeface';
+import * as tfconv from '@tensorflow/tfjs-converter';
+import * as tf from '@tensorflow/tfjs-core';
+
+import {MESH_ANNOTATIONS} from './keypoints';
+import {Pipeline, Prediction} from './pipeline';
+
+const FACEMESH_GRAPHMODEL_PATH =
+    'https://tfhub.dev/mediapipe/tfjs-model/facemesh/1/default/1';
+const MESH_MODEL_INPUT_WIDTH = 192;
+const MESH_MODEL_INPUT_HEIGHT = 192;
+
+interface AnnotatedPredictionValues {
+  /** Probability of the face detection. */
+  faceInViewConfidence: number;
+  boundingBox: {
+    /** The upper left-hand corner of the face. */
+    topLeft: [number, number],
+    /** The lower right-hand corner of the face. */
+    bottomRight: [number, number]
+  };
+  /** Facial landmark coordinates. */
+  mesh: Array<[number, number, number]>;
+  /** Facial landmark coordinates normalized to input dimensions. */
+  scaledMesh: Array<[number, number, number]>;
+  /** Annotated keypoints. */
+  annotations?: {[key: string]: Array<[number, number, number]>};
+}
+
+interface AnnotatedPredictionTensors {
+  faceInViewConfidence: number;
+  boundingBox: {topLeft: tf.Tensor1D, bottomRight: tf.Tensor1D};
+  mesh: tf.Tensor2D;
+  scaledMesh: tf.Tensor2D;
+}
+
+// The object returned by facemesh describing a face found in the input.
+export type AnnotatedPrediction =
+    AnnotatedPredictionValues|AnnotatedPredictionTensors;
+
+/**
+ * Load the model.
+ *
+ * @param options - a configuration object with the following properties:
+ *  - `maxContinuousChecks` How many frames to go without running the bounding
+ * box detector. Only relevant if maxFaces > 1. Defaults to 5.
+ *  - `detectionConfidence` Threshold for discarding a prediction. Defaults to
+ * 0.9.
+ *  - `maxFaces` The maximum number of faces detected in the input. Should be
+ * set to the minimum number for performance. Defaults to 10.
+ *  - `iouThreshold` A float representing the threshold for deciding whether
+ * boxes overlap too much in non-maximum suppression. Must be between [0, 1].
+ * Defaults to 0.3.
+ *  - `scoreThreshold` A threshold for deciding when to remove boxes based
+ * on score in non-maximum suppression. Defaults to 0.75.
+ */
+export async function load({
+  maxContinuousChecks = 5,
+  detectionConfidence = 0.9,
+  maxFaces = 10,
+  iouThreshold = 0.3,
+  scoreThreshold = 0.75
+} = {}): Promise<FaceMesh> {
+  const [blazeFace, blazeMeshModel] = await Promise.all([
+    loadDetectorModel(maxFaces, iouThreshold, scoreThreshold), loadMeshModel()
+  ]);
+
+  const faceMesh = new FaceMesh(
+      blazeFace, blazeMeshModel, maxContinuousChecks, detectionConfidence,
+      maxFaces);
+  return faceMesh;
+}
+
+async function loadDetectorModel(
+    maxFaces: number, iouThreshold: number,
+    scoreThreshold: number): Promise<blazeface.BlazeFaceModel> {
+  return blazeface.load({maxFaces, iouThreshold, scoreThreshold});
+}
+
+async function loadMeshModel(): Promise<tfconv.GraphModel> {
+  return tfconv.loadGraphModel(FACEMESH_GRAPHMODEL_PATH, {fromTFHub: true});
+}
+
+function getInputTensorDimensions(input: tf.Tensor3D|ImageData|HTMLVideoElement|
+                                  HTMLImageElement|
+                                  HTMLCanvasElement): [number, number] {
+  return input instanceof tf.Tensor ? [input.shape[0], input.shape[1]] :
+                                      [input.height, input.width];
+}
+
+function flipFaceHorizontal(
+    face: AnnotatedPrediction, imageWidth: number): AnnotatedPrediction {
+  if (face.mesh instanceof tf.Tensor) {
+    const [topLeft, bottomRight, mesh, scaledMesh] = tf.tidy(() => {
+      const subtractBasis = tf.tensor1d([imageWidth - 1, 0, 0]);
+      const multiplyBasis = tf.tensor1d([1, -1, 1]);
+
+      return tf.tidy(() => {
+        return [
+          tf.concat([
+            tf.sub(
+                imageWidth - 1,
+                (face.boundingBox.topLeft as tf.Tensor1D).slice(0, 1)),
+            (face.boundingBox.topLeft as tf.Tensor1D).slice(1, 1)
+          ]),
+          tf.concat([
+            tf.sub(
+                imageWidth - 1,
+                (face.boundingBox.bottomRight as tf.Tensor1D).slice(0, 1)),
+            (face.boundingBox.bottomRight as tf.Tensor1D).slice(1, 1)
+          ]),
+          tf.sub(subtractBasis, face.mesh).mul(multiplyBasis),
+          tf.sub(subtractBasis, face.scaledMesh).mul(multiplyBasis)
+        ];
+      });
+    });
+
+    return Object.assign(
+        {}, face, {boundingBox: {topLeft, bottomRight}, mesh, scaledMesh});
+  }
+
+  return Object.assign({}, face, {
+    boundingBox: {
+      topLeft: [
+        imageWidth - 1 - (face.boundingBox.topLeft as [number, number])[0],
+        (face.boundingBox.topLeft as [number, number])[1]
+      ],
+      bottomRight: [
+        imageWidth - 1 - (face.boundingBox.bottomRight as [number, number])[0],
+        (face.boundingBox.bottomRight as [number, number])[1]
+      ]
+    },
+    mesh: (face.mesh as Array<[number, number, number]>).map(coord => {
+      const flippedCoord = coord.slice(0);
+      flippedCoord[0] = imageWidth - 1 - coord[0];
+      return flippedCoord;
+    }),
+    scaledMesh:
+        (face.scaledMesh as Array<[number, number, number]>).map(coord => {
+          const flippedCoord = coord.slice(0);
+          flippedCoord[0] = imageWidth - 1 - coord[0];
+          return flippedCoord;
+        })
+  });
+}
+
+export class FaceMesh {
+  private pipeline: Pipeline;
+  private detectionConfidence: number;
+
+  constructor(
+      blazeFace: blazeface.BlazeFaceModel, blazeMeshModel: tfconv.GraphModel,
+      maxContinuousChecks: number, detectionConfidence: number,
+      maxFaces: number) {
+    this.pipeline = new Pipeline(
+        blazeFace, blazeMeshModel, MESH_MODEL_INPUT_WIDTH,
+        MESH_MODEL_INPUT_HEIGHT, maxContinuousChecks, maxFaces);
+
+    this.detectionConfidence = detectionConfidence;
+  }
+
+  static getAnnotations(): {[key: string]: number[]} {
+    return MESH_ANNOTATIONS;
+  }
+
+  /**
+   * Returns an array of faces in an image.
+   *
+   * @param input The image to classify. Can be a tensor, DOM element image,
+   * video, or canvas.
+   * @param returnTensors (defaults to `false`) Whether to return tensors as
+   * opposed to values.
+   * @param flipHorizontal Whether to flip/mirror the facial keypoints
+   * horizontally. Should be true for videos that are flipped by default (e.g.
+   * webcams).
+   *
+   * @return An array of AnnotatedPrediction objects.
+   */
+  async estimateFaces(
+      input: tf.Tensor3D|ImageData|HTMLVideoElement|HTMLImageElement|
+      HTMLCanvasElement,
+      returnTensors = false,
+      flipHorizontal = false): Promise<AnnotatedPrediction[]> {
+    const [, width] = getInputTensorDimensions(input);
+
+    const image: tf.Tensor4D = tf.tidy(() => {
+      if (!(input instanceof tf.Tensor)) {
+        input = tf.browser.fromPixels(input);
+      }
+      return (input as tf.Tensor).toFloat().expandDims(0);
+    });
+
+    // Currently tfjs-core does not pack depthwiseConv because it fails for
+    // very large inputs (https://github.com/tensorflow/tfjs/issues/1652).
+    // TODO(annxingyuan): call tf.enablePackedDepthwiseConv when available
+    // (https://github.com/tensorflow/tfjs/issues/2821)
+    const savedWebglPackDepthwiseConvFlag =
+        tf.env().get('WEBGL_PACK_DEPTHWISECONV');
+    tf.env().set('WEBGL_PACK_DEPTHWISECONV', true);
+    const predictions = await this.pipeline.predict(image);
+    tf.env().set('WEBGL_PACK_DEPTHWISECONV', savedWebglPackDepthwiseConvFlag);
+
+    image.dispose();
+
+    if (predictions != null && predictions.length > 0) {
+      return Promise.all(predictions.map(async (prediction: Prediction, i) => {
+        const {coords, scaledCoords, box, flag} = prediction;
+        let tensorsToRead: tf.Tensor[] = [flag];
+        if (!returnTensors) {
+          tensorsToRead = tensorsToRead.concat(
+              [coords, scaledCoords, box.startPoint, box.endPoint]);
+        }
+
+        const tensorValues = await Promise.all(
+            tensorsToRead.map(async (d: tf.Tensor) => d.array()));
+        const flagValue = tensorValues[0] as number;
+
+        flag.dispose();
+        if (flagValue < this.detectionConfidence) {
+          this.pipeline.clearRegionOfInterest(i);
+        }
+
+        if (returnTensors) {
+          const annotatedPrediction: AnnotatedPrediction = {
+            faceInViewConfidence: flagValue,
+            mesh: coords,
+            scaledMesh: scaledCoords,
+            boundingBox: {
+              topLeft: box.startPoint.squeeze() as tf.Tensor1D,
+              bottomRight: box.endPoint.squeeze() as tf.Tensor1D
+            }
+          };
+
+          if (flipHorizontal) {
+            return flipFaceHorizontal(annotatedPrediction, width);
+          }
+
+          return annotatedPrediction;
+        }
+
+        const [coordsArr, coordsArrScaled, topLeft, bottomRight] =
+                      tensorValues.slice(1) as [
+                        Array<[number, number, number]>,
+                        Array<[number, number, number]>,
+                        [number, number],
+                        [number, number]];
+
+        scaledCoords.dispose();
+        coords.dispose();
+
+        let annotatedPrediction: AnnotatedPredictionValues = {
+          faceInViewConfidence: flagValue,
+          boundingBox: {topLeft, bottomRight},
+          mesh: coordsArr,
+          scaledMesh: coordsArrScaled
+        };
+
+        if (flipHorizontal) {
+          annotatedPrediction =
+              flipFaceHorizontal(annotatedPrediction, width) as
+              AnnotatedPredictionValues;
+        }
+
+        const annotations:
+            {[key: string]: Array<[number, number, number]>} = {};
+        for (const key in MESH_ANNOTATIONS) {
+          annotations[key] = MESH_ANNOTATIONS[key].map(
+              index => annotatedPrediction.scaledMesh[index]);
+        }
+        annotatedPrediction['annotations'] = annotations;
+
+        return annotatedPrediction;
+      }));
+    }
+
+    return [];
+  }
+}
