b72c10bdbdec6b04a13f780180ed904736fa52a5
Ping Yu <4018+pyu10055@users.noreply.github.com>
2020-04-06 14:58:26 -0700

    fix coco-ssd warmup run dtype error (#433)

--- coco-ssd/src/index.ts
+++ coco-ssd/src/index.ts
@@ -87,8 +87,8 @@ export class ObjectDetection {
     this.model = await tfconv.loadGraphModel(this.modelPath);
 
     // Warmup the model.
-    const result = await this.model.executeAsync(tf.zeros([1, 300, 300, 3])) as
-        tf.Tensor[];
+    const result = await this.model.executeAsync(
+                       tf.zeros([1, 300, 300, 3], 'int32')) as tf.Tensor[];
     await Promise.all(result.map(t => t.data()));
     result.map(t => t.dispose());
   }
