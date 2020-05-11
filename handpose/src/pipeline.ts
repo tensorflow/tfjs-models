30db63f837f22115809c8bf966f91df12bff3883
Yannick Assogba <yassogba@google.com>
2020-05-11 15:36:44 -0400

    disable webgpu

--- handpose/src/pipeline.ts
+++ handpose/src/pipeline.ts
@@ -22,7 +22,7 @@ import {Box, cutBoxFromImageAndResize, enlargeBox, getBoxCenter, getBoxSize, shi
 import {HandDetector} from './hand';
 import {rotate as rotateCpu} from './rotate_cpu';
 import {rotate as rotateWebgl} from './rotate_gpu';
-import {rotate as rotateWebgpu} from './rotate_webgpu';
+// import {rotate as rotateWebgpu} from './rotate_webgpu';
 import {buildRotationMatrix, computeRotation, dot, invertTransformMatrix, rotatePoint, TransformationMatrix} from './util';
 
 const UPDATE_REGION_OF_INTEREST_IOU_THRESHOLD = 0.8;
@@ -188,8 +188,8 @@ export class HandPipeline {
 
     if (backend.match('webgl')) {
       rotatedImage = rotateWebgl(image, angle, 0, palmCenterNormalized);
-    } else if (backend === 'webgpu') {
-      rotatedImage = rotateWebgpu(image, angle, 0, palmCenterNormalized);
+      // } else if (backend === 'webgpu') {
+      //   rotatedImage = rotateWebgpu(image, angle, 0, palmCenterNormalized);
     } else if (backend === 'cpu' || backend === 'tensorflow') {
       rotatedImage = rotateCpu(image, angle, 0, palmCenterNormalized);
     } else {
