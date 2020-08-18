1e559fea38aa1398caa648064de8992ed1879b00
Ann Yuan <annyuan@gmail.com>
2020-08-18 13:23:03 -0400

    video build

--- facemesh/demo/video.js
+++ facemesh/demo/video.js
@@ -22,8 +22,6 @@ import '@tensorflow/tfjs-backend-webgl';
 
 import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 
-import {TRIANGULATION} from './triangulation';
-
 tfjsWasm.setWasmPath(
     `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/tfjs-backend-wasm.wasm`);
 
@@ -33,21 +31,7 @@ function isMobile() {
   return isAndroid || isiOS;
 }
 
-function drawPath(ctx, points, closePath) {
-  const region = new Path2D();
-  region.moveTo(points[0][0], points[0][1]);
-  for (let i = 1; i < points.length; i++) {
-    const point = points[i];
-    region.lineTo(point[0], point[1]);
-  }
-
-  if (closePath) {
-    region.closePath();
-  }
-  ctx.stroke(region);
-}
-
-let model, ctx, videoWidth, videoHeight, video, canvas,
+let model, videoWidth, videoHeight, video,
     scatterGLHasInitialized = false, scatterGL;
 
 const VIDEO_SIZE = 500;
@@ -89,19 +73,6 @@ function setupDatGui() {
 
 async function setupCamera() {
   video = document.getElementById('video');
-
-  const stream = await navigator.mediaDevices.getUserMedia({
-    'audio': false,
-    'video': {
-      facingMode: 'user',
-      // Only setting the video to a specified size in order to accommodate a
-      // point cloud, so on mobile devices accept the default size.
-      width: mobile ? undefined : VIDEO_SIZE,
-      height: mobile ? undefined : VIDEO_SIZE
-    },
-  });
-  video.srcObject = stream;
-
   return new Promise((resolve) => {
     video.onloadedmetadata = () => {
       resolve(video);
@@ -113,40 +84,7 @@ async function renderPrediction() {
   stats.begin();
 
   const predictions = await model.estimateFaces(video);
-  ctx.drawImage(
-      video, 0, 0, videoWidth, videoHeight, 0, 0, canvas.width, canvas.height);
-
   if (predictions.length > 0) {
-    predictions.forEach(prediction => {
-      const keypoints = prediction.scaledMesh;
-
-      if (state.triangulateMesh) {
-        for (let i = 0; i < TRIANGULATION.length / 3; i++) {
-          const points = [
-            TRIANGULATION[i * 3], TRIANGULATION[i * 3 + 1],
-            TRIANGULATION[i * 3 + 2]
-          ].map(index => keypoints[index]);
-
-          drawPath(ctx, points, true);
-        }
-      } else {
-        for (let i = 0; i < keypoints.length; i++) {
-          const x = keypoints[i][0];
-          const y = keypoints[i][1];
-
-          ctx.beginPath();
-          ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
-
-          if(i >= 468) {
-            ctx.fillStyle = 'red';
-          } else {
-            ctx.fillStyle = '#32EEDB';
-          }
-          ctx.fill();
-        }
-      }
-    });
-
     if (renderPointcloud && state.renderPointcloud && scatterGL != null) {
       const pointsData = predictions.map(prediction => {
         let scaledMesh = prediction.scaledMesh;
@@ -186,24 +124,13 @@ async function main() {
   document.getElementById('main').appendChild(stats.dom);
 
   await setupCamera();
-  video.play();
-  videoWidth = video.videoWidth;
-  videoHeight = video.videoHeight;
+  videoWidth = 500;
+  videoHeight = 500;
   video.width = videoWidth;
   video.height = videoHeight;
 
-  canvas = document.getElementById('output');
-  canvas.width = videoWidth;
-  canvas.height = videoHeight;
   const canvasContainer = document.querySelector('.canvas-wrapper');
   canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
-
-  ctx = canvas.getContext('2d');
-  ctx.translate(canvas.width, 0);
-  ctx.scale(-1, 1);
-  ctx.fillStyle = '#32EEDB';
-  ctx.strokeStyle = '#32EEDB';
-  ctx.lineWidth = 0.5;
 
   model = await facemesh.load({maxFaces: state.maxFaces});
   renderPrediction();
