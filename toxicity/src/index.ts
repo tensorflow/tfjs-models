fdf1ba90d7a34cb59c03107cf3f0e7171b0552d5
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2020-09-09 11:33:11 -0300

    [toxicity] feat: make toxicityLabels optionally string

--- toxicity/src/index.ts
+++ toxicity/src/index.ts
@@ -30,12 +30,17 @@ declare interface ModelInputs extends tf.NamedTensorMap {
  *
  * @param threshold A prediction is considered valid only if its confidence
  * exceeds the threshold. Defaults to 0.85.
- * @param toxicityLabels An array of strings indicating which types of toxicity
+ * @param toxicityLabels  A string or an array of strings indicating which types of toxicity
  * to detect. Labels must be one of `toxicity` | `severe_toxicity` |
  * `identity_attack` | `insult` | `threat` | `sexual_explicit` | `obscene`.
  * Defaults to all labels.
  */
-export async function load(threshold: number, toxicityLabels: string[]) {
+export async function load(threshold: number, toxicityLabels: string[] | string) {
+
+  if (typeof toxicityLabels === 'string') {
+    toxicityLabels = [toxicityLabels];
+  }
+
   const model = new ToxicityClassifier(threshold, toxicityLabels);
   await model.load();
   return model;
