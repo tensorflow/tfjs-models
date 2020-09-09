9b0fbd06a840d091265c803223fe559880bd0d9e
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2020-09-09 14:27:42 -0300

    fix: comment line removed

--- toxicity/src/index.ts
+++ toxicity/src/index.ts
@@ -18,7 +18,7 @@
 import * as use from '@tensorflow-models/universal-sentence-encoder';
 import * as tfconv from '@tensorflow/tfjs-converter';
 import * as tf from '@tensorflow/tfjs-core';
-export {version} from './version';
+export { version } from './version';
 
 declare interface ModelInputs extends tf.NamedTensorMap {
   Placeholder_1: tf.Tensor;
@@ -30,10 +30,11 @@ declare interface ModelInputs extends tf.NamedTensorMap {
  *
  * @param threshold A prediction is considered valid only if its confidence
  * exceeds the threshold. Defaults to 0.85.
- * @param toxicityLabels  A string or an array of strings indicating 
+ * @param toxicityLabels  A string or an array of strings indicating
  * which types of toxicity to detect. Labels must be one of
  * `toxicity` | `severe_toxicity` | `identity_attack` | `insult` |
  * `threat` | `sexual_explicit` | `obscene`.
+ * Defaults to all labels.
  */
 export async function load(threshold: number, toxicityLabels: string[] | string) {
 
@@ -60,8 +61,8 @@ export class ToxicityClassifier {
 
   async loadModel() {
     return tfconv.loadGraphModel(
-        'https://tfhub.dev/tensorflow/tfjs-model/toxicity/1/default/1',
-        {fromTFHub: true});
+      'https://tfhub.dev/tensorflow/tfjs-model/toxicity/1/default/1',
+      { fromTFHub: true });
   }
 
   async loadTokenizer() {
@@ -70,22 +71,22 @@ export class ToxicityClassifier {
 
   async load() {
     const [model, tokenizer] =
-        await Promise.all([this.loadModel(), this.loadTokenizer()]);
+      await Promise.all([this.loadModel(), this.loadTokenizer()]);
 
     this.model = model;
     this.tokenizer = tokenizer;
 
     this.labels =
-        model.outputs.map((d: {name: string}) => d.name.split('/')[0]);
+      model.outputs.map((d: { name: string }) => d.name.split('/')[0]);
 
     if (this.toxicityLabels.length === 0) {
       this.toxicityLabels = this.labels;
     } else {
       tf.util.assert(
-          this.toxicityLabels.every(d => this.labels.indexOf(d) > -1),
-          () => `toxicityLabels argument must contain only items from the ` +
-              `model heads ${this.labels.join(', ')}, ` +
-              `got ${this.toxicityLabels.join(', ')}`);
+        this.toxicityLabels.every(d => this.labels.indexOf(d) > -1),
+        () => `toxicityLabels argument must contain only items from the ` +
+          `model heads ${this.labels.join(', ')}, ` +
+          `got ${this.toxicityLabels.join(', ')}`);
     }
   }
 
@@ -97,9 +98,9 @@ export class ToxicityClassifier {
    *
    * @param inputs A string or an array of strings to classify.
    */
-  async classify(inputs: string[]|string): Promise<Array<{
+  async classify(inputs: string[] | string): Promise<Array<{
     label: string,
-    results: Array<{probabilities: Float32Array, match: boolean}>
+    results: Array<{ probabilities: Float32Array, match: boolean }>
   }>> {
     if (typeof inputs === 'string') {
       inputs = [inputs];
@@ -110,16 +111,16 @@ export class ToxicityClassifier {
     // const encodings = inputs.map(d => padInput(this.tokenizer.encode(d)));
 
     const indicesArr =
-        encodings.map((arr, i) => arr.map((d, index) => [i, index]));
+      encodings.map((arr, i) => arr.map((d, index) => [i, index]));
 
     let flattenedIndicesArr: Array<[number, number]> = [];
     for (let i = 0; i < indicesArr.length; i++) {
       flattenedIndicesArr =
-          flattenedIndicesArr.concat(indicesArr[i] as Array<[number, number]>);
+        flattenedIndicesArr.concat(indicesArr[i] as Array<[number, number]>);
     }
 
     const indices = tf.tensor2d(
-        flattenedIndicesArr, [flattenedIndicesArr.length, 2], 'int32');
+      flattenedIndicesArr, [flattenedIndicesArr.length, 2], 'int32');
     const values = tf.tensor1d(tf.util.flatten(encodings) as number[], 'int32');
 
     const modelInputs: ModelInputs = {
@@ -133,25 +134,25 @@ export class ToxicityClassifier {
     values.dispose();
 
     return (labels as tf.Tensor2D[])
-        .map((d: tf.Tensor2D, i: number) => ({data: d, headIndex: i}))
-        .filter(
-            (d: {headIndex: number}) =>
-                this.toxicityLabels.indexOf(this.labels[d.headIndex]) > -1)
-        .map((d: {headIndex: number, data: tf.Tensor2D}) => {
-          const prediction = d.data.dataSync() as Float32Array;
-          const results = [];
-          for (let input = 0; input < inputs.length; input++) {
-            const probabilities = prediction.slice(input * 2, input * 2 + 2);
-            let match = null;
+      .map((d: tf.Tensor2D, i: number) => ({ data: d, headIndex: i }))
+      .filter(
+        (d: { headIndex: number }) =>
+          this.toxicityLabels.indexOf(this.labels[d.headIndex]) > -1)
+      .map((d: { headIndex: number, data: tf.Tensor2D }) => {
+        const prediction = d.data.dataSync() as Float32Array;
+        const results = [];
+        for (let input = 0; input < inputs.length; input++) {
+          const probabilities = prediction.slice(input * 2, input * 2 + 2);
+          let match = null;
 
-            if (Math.max(probabilities[0], probabilities[1]) > this.threshold) {
-              match = probabilities[0] < probabilities[1];
-            }
-
-            results.push({probabilities, match});
+          if (Math.max(probabilities[0], probabilities[1]) > this.threshold) {
+            match = probabilities[0] < probabilities[1];
           }
 
-          return {label: this.labels[d.headIndex], results};
-        });
+          results.push({ probabilities, match });
+        }
+
+        return { label: this.labels[d.headIndex], results };
+      });
   }
 }
