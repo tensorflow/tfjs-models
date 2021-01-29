88e303d302acd0f56e13dd99278ef9a356066862
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-01-29 11:49:55 -0800

    fixed the package error

--- speech-commands/rollup.config.js
+++ speech-commands/rollup.config.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
+ * Copyright 2020 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,63 +15,67 @@
  * =============================================================================
  */
 
-import node from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
-import uglify from 'rollup-plugin-uglify';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import {terser} from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
 const PREAMBLE = `/**
- * @license
- * Copyright ${(new Date).getFullYear()} Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */`;
+    * @license
+    * Copyright ${(new Date).getFullYear()} Google LLC. All Rights Reserved.
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    * http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+    * =============================================================================
+    */`;
 
-function minify() {
-  return uglify({output: {preamble: PREAMBLE}});
-}
+function config({ plugins = [], output = {}, tsCompilerOptions = {} }) {
+  const defaultTsOptions = {
+    include: ['src/**/*.ts'],
+    module: 'ES2015',
+  };
+  const tsoptions = Object.assign({}, defaultTsOptions, tsCompilerOptions);
 
-function config({plugins = [], output = {}}) {
   return {
     input: 'src/index.ts',
     plugins: [
-      typescript({tsconfigOverride: {compilerOptions: {module: 'ES2015'}}}),
-      node(), ...plugins
+      typescript(tsoptions),
+      resolve(),
+      commonjs({
+        ignore: ['crypto', 'node-fetch', 'util'],
+        include: 'node_modules/**',
+        namedExports: {
+          './node_modules/seedrandom/index.js': ['alea'],
+        },
+      }),
+      ...plugins
     ],
     output: {
-      banner: PREAMBLE,
       sourcemap: true,
       globals: {'@tensorflow/tfjs': 'tf'},
-      ...output
+      ...output,
     },
     external: ['@tensorflow/tfjs']
   };
 }
 
+const packageName = 'speechCommands';
 export default [
+  config({output: {format: 'umd', name: packageName, file: 'dist/speech-commands.js'}}),
   config({
-    output:
-        {format: 'umd', name: 'speechCommands', file: 'dist/speech-commands.js'}
+    plugins: [terser({output: {preamble: PREAMBLE, comments: false}})],
+    output: {format: 'umd', name: packageName, file: 'dist/speech-commands.min.js'}
   }),
   config({
-    plugins: [minify()],
-    output: {
-      format: 'umd',
-      name: 'speechCommands',
-      file: 'dist/speech-commands.min.js'
-    }
-  }),
-  config({
-    plugins: [minify()],
+    plugins: [terser({output: {preamble: PREAMBLE, comments: false}})],
     output: {format: 'es', file: 'dist/speech-commands.esm.js'}
   })
 ];
