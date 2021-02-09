7f50916d0c17bb47491f7d99e395e61b62c23cbe
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-02-08 19:43:20 -0800

    Update index.ts

--- blazeface/src/index.ts
+++ blazeface/src/index.ts
@@ -15,12 +15,12 @@
  * =============================================================================
  */
 
-import * as tf from "@tensorflow/tfjs-core";
-import * as tfconv from "@tensorflow/tfjs-converter";
-import { BlazeFaceModel } from "./face";
+import * as tf from '@tensorflow/tfjs-core';
+import * as tfconv from '@tensorflow/tfjs-converter';
+import { BlazeFaceModel } from './face';
 
 const BLAZEFACE_MODEL_URL =
-  "https://tfhub.dev/tensorflow/tfjs-model/blazeface/1/default/1";
+  'https://tfhub.dev/tensorflow/tfjs-model/blazeface/1/default/1';
 
 interface BlazeFaceConfig {
   maxFaces?: number;
@@ -71,4 +71,4 @@ export async function load({
   return model;
 }
 
-export { NormalizedFace, BlazeFaceModel, BlazeFacePrediction } from "./face";
+export { NormalizedFace, BlazeFaceModel, BlazeFacePrediction } from './face';
