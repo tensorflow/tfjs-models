4b0cf1b92163c2ec8e4513b9251d764e9ca4dbff
Na Li <linazhao@google.com>
2021-03-11 12:29:55 -0800

    Add base class.

---
+++ pose-detection/src/types.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+import * as tf from '@tensorflow/tfjs-core';
+
+export enum SupportedPipelines {
+  posenet = 'posenet'
+}
+
+/**
+ * Common config to create the pose detector.
+ */
+export interface PipelineConfig {
+  inputResolution?: number;
+  quantBytes?: number;
+}
+
+/**
+ * Allowed input format for the `estimatePoses` method.
+ */
+export type PoseDetectorInput =
+    tf.Tensor3D|ImageData|HTMLVideoElement|HTMLImageElement|HTMLCanvasElement;
+
+/**
+ * Common config for the `estimatePoses` method.
+ */
+export interface EstimationConfig {
+  maxPoses?: number;
+  flipHorizontal?: boolean;
+  smoothLandmarks?: boolean;
+  maxContinousChecks?: number;
+}
+
+/**
+ * A keypoint that contains coordinate information.
+ */
+export interface Keypoint {
+  x: number, y: number
+}
