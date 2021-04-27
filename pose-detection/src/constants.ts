c1717e4cbee1445a2ea15ac0627e7866599c561a
Na Li <linazhao@google.com>
2021-04-27 11:26:15 -0700

    Address comments.

--- pose-detection/src/constants.ts
+++ pose-detection/src/constants.ts
@@ -14,14 +14,16 @@
  * limitations under the License.
  * =============================================================================
  */
-// Don't change the order.
+// Don't change the order. The order needs to be consistent with the model
+// keypoint result list.
 export const COCO_KEYPOINTS = [
   'nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear', 'left_shoulder',
   'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist',
   'left_hip', 'right_hip', 'left_knee', 'right_knee', 'left_ankle',
   'right_ankle'
 ];
-// Don't change the order.
+// Don't change the order. The order needs to be consistent with the model
+// keypoint result list.
 export const BLAZEPOSE_KEYPOINTS = [
   'nose',           'right_eye_inner', 'right_eye',      'right_eye_outer',
   'left_eye_inner', 'left_eye',        'left_eye_outer', 'right_ear',
