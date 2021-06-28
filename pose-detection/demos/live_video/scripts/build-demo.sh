4e1cbef00e5b756340728f22108cd1f57f038171
Matthew Soulanille <msoulanille@google.com>
2021-06-28 11:01:26 -0700

    [pose-detection] Use esbuild for dev and prod demo bundles

---
+++ pose-detection/demos/live_video/scripts/build-demo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Copyright 2021 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+set -e
+
+yarn
+yarn rimraf dist/
+yarn esbuild src/index.js --sourcemap --minify --target=es6 --sources-content=true --external:os --bundle --resolve-extensions=.mjs,.js --outdir=dist/
+cp src/index.html dist/
+
+echo "Demo built successfully"
