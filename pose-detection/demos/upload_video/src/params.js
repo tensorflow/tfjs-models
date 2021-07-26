436832ca9158aa5f7c3ac721f0069ef372dd2ded
Yunfei Hao <yunfei.hao@intel.com>
2021-07-26 10:21:43 +0800

    Add webgpu backend for demos in pose-detection

--- pose-detection/demos/upload_video/src/params.js
+++ pose-detection/demos/upload_video/src/params.js
@@ -70,13 +70,14 @@ export const BACKEND_FLAGS_MAP = {
     'WEBGL_FORCE_F16_TEXTURES', 'WEBGL_RENDER_FLOAT32_CAPABLE',
     'WEBGL_FLUSH_THRESHOLD'
   ],
+  ['tfjs-webgpu']: [],
   ['mediapipe-gpu']: []
 };
 
 export const MODEL_BACKEND_MAP = {
-  [posedetection.SupportedModels.PoseNet]: ['tfjs-webgl'],
-  [posedetection.SupportedModels.MoveNet]: ['tfjs-webgl', 'tfjs-wasm'],
-  [posedetection.SupportedModels.BlazePose]: ['mediapipe-gpu', 'tfjs-webgl']
+  [posedetection.SupportedModels.PoseNet]: ['tfjs-webgl', 'tfjs-webgpu'],
+  [posedetection.SupportedModels.MoveNet]: ['tfjs-webgl', 'tfjs-wasm', 'tfjs-webgpu'],
+  [posedetection.SupportedModels.BlazePose]: ['mediapipe-gpu', 'tfjs-webgl', 'tfjs-webgpu']
 }
 
 export const TUNABLE_FLAG_NAME_MAP = {
