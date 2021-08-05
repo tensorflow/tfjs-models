a8320c9d4f2811635aaf33b0e940dab54569499b
Na Li <linazhao@google.com>
2021-08-05 16:29:51 -0700

    Test iOS.

--- pose-detection/demos/live_video/src/params.js
+++ pose-detection/demos/live_video/src/params.js
@@ -85,8 +85,7 @@ export const BACKEND_FLAGS_MAP = {
 export const MODEL_BACKEND_MAP = {
   [posedetection.SupportedModels.PoseNet]: ['tfjs-webgl'],
   [posedetection.SupportedModels.MoveNet]: ['tfjs-webgl', 'tfjs-wasm'],
-  [posedetection.SupportedModels.BlazePose]:
-      isiOS() ? ['tfjs-webgl'] : ['mediapipe-gpu', 'tfjs-webgl']
+  [posedetection.SupportedModels.BlazePose]: ['mediapipe-gpu', 'tfjs-webgl']
 }
 
 export const TUNABLE_FLAG_NAME_MAP = {
