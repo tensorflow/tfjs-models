5a9a6021fb620d88f3e9837d4f39c7f788e7306a
Ahmed Sabie <ahmedsabie@google.com>
2021-08-13 02:03:29 -0400

    Rename variables

--- pose-detection/src/blazepose_tfjs/calculators/calculate_score_copy.ts
+++ pose-detection/src/blazepose_tfjs/calculators/calculate_score_copy.ts
@@ -18,27 +18,28 @@
 import {Keypoint} from '../../types';
 
 /**
- * A calculator to copy visibility between landmarks.
+ * A calculator to copy score between landmarks.
  *
  * Landmarks to copy from and to copy to can be of different type (normalized or
  * non-normalized), but landmarks to copy to and output landmarks should be of
  * the same type.
- * @param landmarksFrom  A LandmarkList or NormalizedLandmarklist of landmarks
+ * @param landmarksFrom  A list of landmarks.
  *     to copy from.
- * @param landmarksTo  A LandmarkList or NormalizedLandmarklist of landmarks
+ * @param landmarksTo  A list of landmarks.
  *     to copy to.
+ * @param copyScore Copy the score from the `landmarksFrom` parameter.
  */
 // ref:
 // https://github.com/google/mediapipe/blob/master/mediapipe/calculators/util/visibility_copy_calculator.cc
-export function calculateVisibilityCopy(
-    landmarksFrom: Keypoint[], landmarksTo: Keypoint[], copyVisibility = true) {
+export function calculateScoreCopy(
+    landmarksFrom: Keypoint[], landmarksTo: Keypoint[], copyScore = true) {
   const outputLandmarks = [];
   for (let i = 0; i < landmarksFrom.length; i++) {
     // Create output landmark and copy all fields from the `to` landmarks
     const newLandmark = {...landmarksTo[i]};
 
-    // Copy visibility and presence from the `from` landmark.
-    if (copyVisibility) {
+    // Copy score from the `from` landmark.
+    if (copyScore) {
       newLandmark.score = landmarksFrom[i].score;
     }
 
