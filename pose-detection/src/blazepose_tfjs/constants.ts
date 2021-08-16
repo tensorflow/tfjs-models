b486796e3f344015adbce1f914391f52830cd935
Ahmed Sabie <ahmedsabie@google.com>
2021-08-16 13:30:20 -0400

    Add 3D keypoints output to TFJS blazepose

--- pose-detection/src/blazepose_tfjs/constants.ts
+++ pose-detection/src/blazepose_tfjs/constants.ts
@@ -97,6 +97,11 @@ export const BLAZEPOSE_TENSORS_TO_LANDMARKS_CONFIG = {
   inputImageWidth: 256,
   inputImageHeight: 256
 };
+export const BLAZEPOSE_TENSORS_TO_WORLD_LANDMARKS_CONFIG = {
+  numLandmarks: 39,
+  inputImageWidth: 1,
+  inputImageHeight: 1
+};
 export const BLAZEPOSE_REFINE_LANDMARKS_FROM_HEATMAP_CONFIG = {
   kernelSize: 7,
   minConfidenceToRefine: 0.5
@@ -132,5 +137,19 @@ export const BLAZEPOSE_LANDMARKS_SMOOTHING_CONFIG_AUXILIARY = {
     derivateCutOff: 1.0,  // derivateCutOff 1.0 results into ~0.17 alpha in
                           // landmark velocity EMA filter.
     minAllowedObjectScale: 1e-6
+  }
+};
+export const BLAZEPOSE_WORLD_LANDMARKS_SMOOTHING_CONFIG_ACTUAL = {
+  oneEuroFilter: {
+    frequency: 30,
+    minCutOff: 0.1,  // Min cutoff 0.1 results into ~ 0.02 alpha in landmark EMA
+                     // filter when landmark is static.
+    beta:
+        40,  // Beta 40.0 in combintation with min_cutoff 0.1 results into ~0.8
+             // alpha in landmark EMA filter when landmark is moving fast.
+    derivateCutOff: 1.0,  // Derivative cutoff 1.0 results into ~0.17 alpha in
+                          // landmark velocity EMA filter.
+    minAllowedObjectScale: 1e-6,
+    disableValueScaling: true
   }
 };
