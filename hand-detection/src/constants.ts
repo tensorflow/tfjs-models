a05f9bff42a82a0a4afb70f7f4f50ad61e91c715
ahmedsabie <ahmedsabie@google.com>
2021-09-27 15:21:41 -0400

    Add MediaPipe Hands implementation (#839)
    * Add MediaPipe Hands implementation
    
    * Edit readmes
    
    * Fix z scaling in landmark

--- hand-detection/src/constants.ts
+++ hand-detection/src/constants.ts
@@ -15,11 +15,26 @@
  * =============================================================================
  */
 
-export {createDetector} from './create_detector';
-// HandDetector class.
-export {HandDetector} from './hand_detector';
-// Entry point to create a new detector instance.
-export {MediaPipeHandsMediaPipeEstimationConfig, MediaPipeHandsMediaPipeModelConfig} from './mediapipe/types';
-
-// Supported models enum.
-export * from './types';
+export const MEDIAPIPE_KEYPOINTS = [
+  'wrist',
+  'thumb_cmc',
+  'thumb_mcp',
+  'thumb_ip',
+  'thumb_tip',
+  'index_finger_mcp',
+  'index_finger_pip',
+  'index_finger_dip',
+  'index_finger_tip',
+  'middle_finger_mcp',
+  'middle_finger_pip',
+  'middle_finger_dip',
+  'middle_finger_tip',
+  'ring_finger_mcp',
+  'ring_finger_pip',
+  'ring_finger_dip',
+  'ring_finger_tip',
+  'pinky_finger_mcp',
+  'pinky_finger_pip',
+  'pinky_finger_dip',
+  'pinky_finger_tip',
+];
