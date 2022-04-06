5923ce63a17feff62719b5ef83483ec17bb8f18f
ahmedsabie <ahmedsabie@google.com>
2022-04-06 14:54:02 -0400

    Add face landmarks detection upload video demo (#998)
    * Add face landmarks detection upload video demo
    
    * Fix copyright date
    
    Co-authored-by: Na Li <linazhao@google.com>

---
+++ face-landmarks-detection/demos/upload_video/src/camera.js
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {drawResults} from './shared/util';
+
+export class Context {
+  constructor() {
+    this.video = document.getElementById('video');
+    this.canvas = document.getElementById('output');
+    this.source = document.getElementById('currentVID');
+    this.ctx = this.canvas.getContext('2d');
+    const stream = this.canvas.captureStream();
+    const options = {mimeType: 'video/webm; codecs=vp9'};
+    this.mediaRecorder = new MediaRecorder(stream, options);
+    this.mediaRecorder.ondataavailable = this.handleDataAvailable;
+  }
+
+  drawCtx() {
+    this.ctx.drawImage(
+        this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
+  }
+
+  clearCtx() {
+    this.ctx.clearRect(0, 0, this.video.videoWidth, this.video.videoHeight);
+  }
+
+  /**
+   * Draw the keypoints on the video.
+   * @param faces A list of faces to render.
+   */
+  drawResults(faces, triangulateMesh, boundingBox) {
+    drawResults(this.ctx, faces, triangulateMesh, boundingBox);
+  }
+}
