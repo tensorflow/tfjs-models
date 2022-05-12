5ea2de3cc1b65e961159042a116b00e24f2681e0
Ahmed Sabie <ahmedsabie@google.com>
2022-05-12 16:13:03 -0400

    Add depth map demo

---
+++ depth-estimation/demos/depth_map/js/dat-gui.js
@@ -0,0 +1,50 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+async function setupDatGui() {
+  const gui = new dat.GUI({width: 300});
+  gui.domElement.id = 'gui';
+
+  // The shader folder contains parameters for the shader.
+  const shaderFolder = gui.addFolder('Shader');
+
+  showShaderConfigs(shaderFolder);
+
+  shaderFolder.open();
+
+  const modelFolder = gui.addFolder('Model')
+
+  modelFolder.add(STATE, 'DepthCachedFrames', 1, 60).step(1);
+
+  modelFolder.open();
+
+  return gui;
+}
+
+async function showShaderConfigs(folderController) {
+  // Clean up shader configs for the previous model.
+  const fixedSelectionCount = 0;
+  while (folderController.__controllers.length > fixedSelectionCount) {
+    folderController.remove(
+        folderController
+            .__controllers[folderController.__controllers.length - 1]);
+  }
+
+  for (const uniform of shaderUniforms) {
+    folderController.add(STATE, uniform[0], uniform[1], uniform[2]);
+  }
+}
