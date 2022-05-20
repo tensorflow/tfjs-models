a1a8df7858cfb876c2f7908a6debf95552809b96
Matthew Soulanille <msoulanille@google.com>
2022-05-20 16:54:20 -0700

    [face-landmarks-detection] Redirect to facemesh when query string is bad (#1021)

--- face-landmarks-detection/demos/shared/option_panel.js
+++ face-landmarks-detection/demos/shared/option_panel.js
@@ -40,7 +40,11 @@ export async function setupModelFolder(gui, urlParams) {
           faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
       break;
     default:
-      alert(`${urlParams.get('model')}`);
+      alert(`${urlParams.get('model')} is not a supported model. `
+	    + 'Redirecting to mediapipe_face_mesh');
+      const url = new URL(window.location.href);
+      url.searchParams.set('model', 'mediapipe_face_mesh');
+      window.location.href = url;
       break;
   }
 
