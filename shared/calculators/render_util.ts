c7b61ab6cc34ec5a5fe7e067a3c84e8983bca122
Yunfei Hao <yunfei.hao@intel.com>
2022-10-27 14:41:54 +0800

    Fix rollup build error after typescript update to 4.8.4

--- shared/calculators/render_util.ts
+++ shared/calculators/render_util.ts
@@ -25,7 +25,7 @@ import {Color, PixelInput, Segmentation} from './interfaces/common_interfaces';
  * It is adapted to account for the generic segmentation interface.
  */
 
-type ImageType = CanvasImageSource|PixelInput;
+type ImageType = CanvasImageSource|OffscreenCanvas|PixelInput;
 type HasDimensions = {
   width: number,
   height: number
@@ -180,7 +180,7 @@ function flipCanvasHorizontal(canvas: Canvas) {
 async function drawWithCompositing(
     ctx: CanvasRenderingContext2D|OffscreenCanvasRenderingContext2D,
     image: ImageType, compositeOperation: string) {
-  ctx.globalCompositeOperation = compositeOperation;
+  ctx.globalCompositeOperation = compositeOperation as GlobalCompositeOperation;
   await drawImage(ctx, image, 0, 0);
 }
 
